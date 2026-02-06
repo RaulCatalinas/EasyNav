e733c7853efe885e4687e38847cf73da63fe4b14
Raul Catalinas <raulcatalinas@gmail.com>
2026-02-06 01:10:24 +0100

    Library Initial Commit

---
+++ easynav-tab/src/main/java/io/github/raulcatalinas/easynav_tab/EasyTabNav.kt
@@ -0,0 +1,9 @@
+package io.github.raulcatalinas.easynav_tab
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EasyTabNav() {
+    Log.i("EasyNav", "Hello World")
+}
